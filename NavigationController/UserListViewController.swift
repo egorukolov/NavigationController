350498d9989fb17bf8d4685190f21af4c5637719
Egor Ukolov <ukolovegor@yandex.ru>
2022-12-26 22:47:18 +0700

    Создание экранов с Navigation Controller

---
+++ NavigationController/UserListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserListViewController.swift
+//  NavigationController
+//
+//  Created by Egor Ukolov on 19.12.2022.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var users = ["Rick", "Morty"]
+
+}
